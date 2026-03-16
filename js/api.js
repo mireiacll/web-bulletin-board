25dd1d50240890afd41e8b453a55d970812e2bbb
Mireia <mireia@gaia3d.com>
2026-03-17 08:19:21 +0900

    main files structure and main board
    * general structure of files and folders done
    * main page logic done (without css): accessed the database and retrieved the posts and users

---
+++ js/api.js
@@ -0,0 +1,34 @@
+// handle all the HTTP requests
+const API_BASE_URL = "https://jsonplaceholder.typicode.com"
+
+async function getPosts() {
+    const response =  await fetch(`${API_BASE_URL}/posts`);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    return response.json();
+}
+
+async function getUsers() {
+    const response =  await fetch(`${API_BASE_URL}/users`);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    return response.json();
+}
+
+async function getPost(id){
+    const response = await fetch(`${API_BASE_URL}/posts/${id}`);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    return response.json();
+}
+
+async function getComments(postId){
+    const response = await fetch(`${API_BASE_URL}/comments?postId=${postId}`);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    return response.json();
+}
---
+++ js/main.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded",init);
+
+async function init(){
+    const posts = await getPosts();
+    const users = await getUsers();
+    displayPosts(posts,users);
+}
+
+function displayPosts(posts,users){
+    const tableBody = document.getElementById("postTableBody");
+    posts.forEach(post => {
+        const user = users.find(u => u.id ===post.userId);
+
+        const row = document.createElement("tr");
+
+        const idCell = document.createElement("td");
+        idCell.textContent=post.id;
+
+        const titleCell = document.createElement("td");
+        const titleLink = document.createElement("a");
+        titleLink.textContent = post.title;
+        titleLink.href = "post.html?id="+post.id;
+        titleCell.appendChild(titleLink);
+
+        const usernameCell = document.createElement("td");
+        usernameCell.textContent = user.username;
+
+        row.appendChild(idCell);
+        row.appendChild(titleCell);
+        row.appendChild(usernameCell);
+
+        tableBody.appendChild(row);
+    });
+}
